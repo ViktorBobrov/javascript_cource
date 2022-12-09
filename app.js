43280d7068c62226aea65146df68a800d9728a7c
ViktorBobrov <bobrov.voktor.93@gmail.com>
2022-12-09 14:59:07 +0700

    init:operators

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+//операторы присваивания
+let age = 18+5;
+age +=2;//age = age+2
+age -=3;//age = age-3
+age *=2;//age = age*2
+age /=2;//age = age/2
+console.log(age);
+//операторы сравнения
+const vasya = 20;
+console.log(age > vasya);
+console.log(age >= vasya);
+console.log(age < vasya);
+console.log(age <= vasya);
+console.log(age == vasya);
