eaadc20fc9962901838c301a1782cb9aef50e4a9
ViktorBobrov <bobrov.voktor.93@gmail.com>
2022-12-10 17:00:15 +0700

    fix:practise

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-//операторы присваивания
-let age = 18+5;
-age +=2;//age = age+2
-age -=3;//age = age-3
-age *=2;//age = age*2
-age /=2;//age = age/2
-console.log(age);
-//операторы сравнения
-const vasya = 20;
-console.log(age > vasya);
-console.log(age >= vasya);
-console.log(age < vasya);
-console.log(age <= vasya);
-console.log(age == vasya);
+/*
+Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+Выведете в консоль:
+Boolean переменную успеете ли вы взяться за работу
+Сколько вы за неё попросите?
+ */
+ let time = (11*5)-(2*5);// число часов на работу
+ let time2 = time>=40;
+ let money = 40* 80;
+ console.log(time2);
+ console.log(money);
