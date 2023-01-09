5b84feb84021bc18bb4ff956577b54bbe9efe56d
ViktorBobrov <bobrov.voktor.93@gmail.com>
2023-01-09 15:35:02 +0700

    fix:practise and method

--- app.js
+++ app.js
@@ -1,139 +1,78 @@
-//filter
+const userName = 'Вася Пупкин';
+// console.log(userName[0]);//выведет в консоль нулевой элемент строки. то есть букву B
+// отдельно для этого придумали  функцию charAt()
+// console.log(userName.charAt(2));
+// как получить длинну строки? с помощью метода length
+// console.log(userName.length);
+// как искать по строке? indexof
+// console.log(userName.indexOf('П'));//возвращает индекс буквы когда она попадается первый раз
+// console.log(userName.lastIndexOf('а'));//вернет последнее вхождение буквы
+// console.log(userName.includes('уп'));//проверяет на наличие такого символа и выдает тру или фалсе
+// console.log(userName.slice(5));
 
-// const operations = [100,-20,7,-20,50];
-//через цикл for(отобразить только положительные приходы);
-// 
-// const positiveOperations =[];// массив что в начале пуст куда будет записываться  новый массив
-// for(const operation of operations){
-// if(operation>0){
-// positiveOperations.push(operation);
-// }
-// }
-// console.log(positiveOperations);
-// const positiveOperations = operations.filter(operation => {
-// return operation > 0;
-// });
-// console.log(positiveOperations);
+// практика
+// const userName2 = 'Вася aka Terminator Пупкин';
+// console.log(userName2.slice(0, 5) + userName2.slice(20, 26));
 
-//forEach
-// const score = [5,10,0,15];//данно массив
-// как сделать через цикл for
-// for(const [i,el] of score.entries()){
-// console.log(`Раунд ${i}:${el}`);
-// }
-// 
-//как через фор ич
-// score.forEach( (el,i)=>{
-// console.log(`Раунд ${i+1}:${el}`);
-// });
-// частичная замена for  цикла удобно использовать когда не хотим что то прерывать, когда итерируясь по элементу использовать индекс
-
-// метод map() массива в програмировании
-// const transactionsInUSD = [10,-7,50,-10,100];
-// const transactionsInRUB = [];
-//нужно перевести стоимость транзакций из долларов  в рубли и заполнить массив  в рублях
-// как сделать это все через обычный for
-// for (const transaction of transactionsInUSD ){
-// transactionsInRUB.push(transaction * 60)
-// }
-// console.log(transactionsInRUB);
-// как это сделать через map()
-// const transactionsInRUB2 = transactionsInUSD.map((transaction,i)=>{
-// return transaction * 60;
-// });
-// map() по сути своей берет исходный массив и преобразовывает его и для каждого элемента выполняет функцию, при этом результат кладет в новый массив
-
-//функция filter()
-// const operations = [100,-20,7,-20,50];
-// const positiveOperations =operations.filter(operation=>{
-// return operation>0;
-// });
-// console.log(positiveOperations);
-// 
-//практика на операции
-/*
-имеется массив изменения цен prises,где внутри первый елемент  массива является ценой в момент X второй в момент Y
-нужно преобразовать данные в макссив,где будут  отображаться только  положительные изменения цен: [100,150]
-*/
-// const prises =[[100,200],[120,100],[280,350]];
-// const result =prises
-// .map(product=>product[1] - product[0])
-// .filter(price => price > 0);
-// console.log(result);
-// метод reduse
-// последовательность чисел свернуть в одно число
-// const operations =[100,-20,7,-30,50];
-// 
-// let balance = 0;
-// for(const operation of operations){
-// balance += operation;
-// };
-// console.log(balance);
-// теперь решить эту задачу методом reduse
-// const finalBalanse = operations.reduce((acc,value)=>{
-// return acc += value;
-// },0);
-
-///практика
-// const arr =[1,4,4,10];
-// 
-// const avg = arr.reduce((acc,el,i)=>{
-// if(i != arr.length -1){
-// return acc + el;
-// }else{
-// return (acc + el)/arr.length;
-// }
-// 
-// },0);
-// console.log(avg);
-
-// const arr = [2,4,4,10];
-// let elGT5;
-// for (const el of arr){
-// if(el > 5){
-// elGT5 = el;
-// break;
-// }
-// };
-// console.log(elGT5);
-// elGT5 = arr.find(el =>el > 5);
-// console.log(elGT5);
-// function some(arr,elem){
-// const res = arr.find(el =>el===elem );
-// return res == undefined ? false : true;
-// }
-// 
-// console.log(arr.some(el =>el===elem));// метод some() проверяет на наличие  элемента в массиве. выводит тру или фалсе. является заменой функции indexof() только indexof выводит -1 и 1
-
-//методы flat() flatMap()
-/*
-метод flat() нужен чтобы превратить массив содержащий массивы в 1 массив без внутренних массивов. при этом элементы подмассива становятся элементами массива */
-// const prises = [[2, 4], [3, 4], [10, [20, 50]]];
-// const res = prises.flat(2);// функция flat уберет внутренние массивы до 2й вложенности и оставит 1 массив.
-// const res2 = prises.flatMap(el => el.concat(1));//в данном случае, функция ведет себя как flat приводя все к плоскому массиву а после как функция map() которая внутрь себя реализовала логику конкатенации подмассивов с помощью 1
-
-//метод sort(),нужен для сортировки
-// const users = ['Вася', 'Катя', 'Маша', 'Аня'];
-// users.sort()// если использовать метод sort() на массив то он будет работать с ними как со строками
-// const operations = [100, -300, -100, 50, 480];
-// operations.sort((a, b) => {// данная логиа используется чтобы отсортировать по возрастанию
-	// if (a > b) {
-		// return 1;
+//преобразование строки
+// const str = 'Вася Пупкин';
+// console.log(str.includes('a'));// показывает есть ли буква а  и выводит true
+// console.log(str.startsWith('В'));// проверяет начинается ли с буквы В и выводит true
+// console.log(str.endsWith('В'));// тоже что и startsWith но с конца
+// console.log(str.toLowerCase());// приведет всю строку к нижнему регистру
+// console.log(str.toUpperCase());//приведет всю строку к верхнему регистру
+// console.log(str.replace('В','Ф'));//Заменяют символ 
+// console.log(str.replaceAll('В','Ф'));// заменяет вообще все символы В на Ф. новый метод не везде работает))
+// console.log(str.replace(/В/g,'Ф'));//получим тоже самое что и replaceAll
+// практика
+// function isPhoneNumber(num){
+	// num =num.trim();
+	// num =num.replace('+7','8');
+	// if(!num.startsWith('8')){
+		// return false;
 	// }
-	// if (a < b) {
-		// return -1;
+	// num = num.replaceAll('(','');
+	// num = num.replaceAll(')','');
+	// num = num.replaceAll(' ','');
+	// num = num.replaceAll('-','');
+	// if(num.length != 11){
+		// return false;
+	// }
+	// let onlyNumber = true;
+	// for(const char of num){
+		// if(isNaN(Number(char))){
+			// onlyNumber = false;
+			// break
+		// }
 	// };
-// });
-// console.log(operations);
+// return onlyNumber;
+// };
+// console.log(isPhoneNumber());
 
-//методы создания массивов
-// const arr = [1, 2, 3, 4, 5, 6];
-// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
-// const arr2 = new Array(5);
-// arr2.fill(1);
-// console.log(arr2);
-// const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1);
+// строки и массивы
 
-let arr = [1, 2, 10, 15];
-arr.sort()
-console.log(arr);
+// const userFullName = 'Вася Пупкин Васильевич';
+// console.log(userFullName.split(' '));// разбивка  строки на массив
+// const [firstName,familyName,lastName] = userFullName.split(' ');
+// console.log(firstName);
+// console.log(familyName);
+// console.log(lastName);
+// 
+// const arr = ['Ты', 'Знаешь', 'JS'];//обьединение массива в строку
+// console.log(arr.join(' '));
+// 
+//дополнение строк
+// const film = 'Звездные войны';
+// console.log(film.padStart(50,'*'));// в начало  добавляет 50 символов и второй аргумент каким именно символом
+// console.log(film.padEnd(50,'*'));//добавляет 50 символов в конец
+// console.log(film.repeat(10));
+// 
+// упражнение макскировка карты
+// const card ='2342834503458353'
+// function safeCard (card){
+// card = card.slice(12,16).padStart(12,'*');
+// console.log(card);
+// };
+// safeCard(card);
+let str = 'How are you?';
+console.log(str.split(' ')[1]);​
